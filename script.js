04d41a84b0a24e2a9f3280cf9cc0b5d296443ec6
shuvenduparida <220301120096@cutm.ac.in>
2024-04-17 23:09:13 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper');
+const Loginlink = document.querySelector('.login-link');
+const Registerlink = document.querySelector('.register-link');
+const btnLogin = document.querySelector('.btnLogin-pop');
+const IconClose = document.querySelector('.icon-close');
+const HeaderWrapper = document.querySelector('.header-wrapper');
+
+Registerlink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+
+Loginlink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnLogin.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+
+IconClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
+
+// btnLogin-pop.onclick(toggleHide());
+
+
+
